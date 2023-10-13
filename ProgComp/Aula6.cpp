edc157bd0d541c356ad5c21f42c60d2bcd961130
Luiz Paulo Colombiano <lpcolombiano@gmail.com>
2023-10-13 15:24:55 -0300

    Update Aula6.cpp

--- ProgComp/Aula6.cpp
+++ ProgComp/Aula6.cpp
@@ -24,3 +24,24 @@ int main()
 	std::cout << " Voce, " << nome << ", e alguem que gosta de comer " << TipoComida << std::endl;
 
 }*/
+
+
+/* #include <iostream>
+
+int main()
+
+{
+	const int TamanhoVetor = 30;
+	char nome[TamanhoVetor];
+	char TipoComida[TamanhoVetor];
+	std::cout << "Escreva seu nome completo: " << std::endl;
+	std::cin.getline(nome, TamanhoVetor);
+	std::cout << "Escreva sua idade: " << std::endl;
+	int idade;
+	std::cin >> idade;
+	std::cin.get();
+	std::cout << "Escreva o nome do tipo de comida favorita: " << std::endl;
+	std::cin.getline(TipoComida, TamanhoVetor);
+	std::cout << " Voce, " << nome << "e alguem que gosta de comer " << TipoComida << " apesar de ter apenas " <<idade<< " anos" <<std::endl;
+
+}*/
