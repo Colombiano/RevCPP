5b275d313e98ba30cc385c39b6c10af857b4993e
Luiz Paulo Colombiano <lpcolombiano@gmail.com>
2023-10-13 15:24:07 -0300

    Create Aula6.cpp

---
+++ ProgComp/Aula6.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+	char nomes[6] = { 'm','a','r','i','a','\0' };
+	char nomezinho[6] = { 'm','a','r','i','a','s'};
+	std::cout << nomes << std::endl;
+	std::cout << nomezinho;
+
+}
+
+/* #include <iostream>
+
+int main()
+
+{
+	const int TamanhoVetor = 30;
+	char nome[TamanhoVetor];
+	char TipoComida[TamanhoVetor];
+	std::cout << "Escreva seu nome completo: " << std::endl;
+	std::cin.getline(nome, TamanhoVetor);
+	std::cout << "Escreva o nome do tipo de comida favorita: " << std::endl;
+	std::cin.getline(TipoComida, TamanhoVetor);
+	std::cout << " Voce, " << nome << ", e alguem que gosta de comer " << TipoComida << std::endl;
+
+}*/
