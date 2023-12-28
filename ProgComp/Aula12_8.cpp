58ac36b03b386fd2e15a18e93cd97163791f1af8
Luiz Paulo Colombiano <lpcolombiano@gmail.com>
2023-12-28 09:37:01 -0300

    Create Aula12_8.cpp

---
+++ ProgComp/Aula12_8.cpp
@@ -0,0 +1,81 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+
+struct complexo
+{
+	float real;
+	float imaginario;
+};
+
+
+complexo soma(complexo, complexo);
+complexo subtracao(complexo, complexo);
+void exibir(complexo);
+complexo ler();
+
+int main()
+
+{
+
+	std::cout << "Digite o primeiro numero complexo" << std::endl;
+	complexo c1 = ler();
+	std::cout << "Digite o segundo numero complexo" << std::endl;
+	complexo c2 = ler();
+	exibir(c1);
+	exibir(c2);
+	complexo c3 = soma(c1, c2);
+	complexo c4 = subtracao(c1, c2);
+	std::cout << "o valor da soma eh :";
+	exibir(c3);
+	std::cout << std::endl;
+	std::cout << "o valor da subtracao eh :";
+	exibir(c4);
+	std::cout << std::endl;
+	
+
+}
+
+
+complexo soma(complexo a, complexo b)
+
+{
+	complexo c;
+	c.real = a.real + b.real;
+	c.imaginario = a.imaginario + b.imaginario;
+	return c;
+
+}
+
+complexo subtracao(complexo a, complexo b)
+
+{
+	complexo c;
+	c.real = a.real - b.real;
+	c.imaginario = a.imaginario - b.imaginario;
+	return c;
+
+}
+
+void exibir(complexo d)
+
+{
+	std::cout << d.real;
+	std::cout << std::showpos;
+	std::cout << d.imaginario;
+	std::cout << std::noshowpos;
+	std::cout << "i" << std::endl;
+
+}
+
+complexo ler ()
+{
+	complexo leitura;
+	std::cin >> leitura.real;
+	std::cin >> leitura.imaginario;
+	std::cin.get();
+	return leitura;
+
+
+}
