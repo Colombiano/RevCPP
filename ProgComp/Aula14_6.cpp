3bae86d51f3fbf8166714fe7cad835dfa72bc116
Luiz Paulo Colombiano <lpcolombiano@gmail.com>
2024-01-04 10:31:22 -0300

    Create Aula14_6.cpp

---
+++ ProgComp/Aula14_6.cpp
@@ -0,0 +1,18 @@
+// Ponteiros de fixação 01
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+
+int main()
+
+{
+
+	int x;
+	int* p = &x;
+	int* q = (int*)p;
+	x = 10;
+	std::cout << *q;
+
+
+}
